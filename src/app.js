3940148067dd828511b7f57b78bedc17631ee215
Dan Trevino <dantrevino@gmail.com>
2017-02-23 23:58:44 -0800

    new library data, update library reducer

--- src/app.js
+++ src/app.js
@@ -3,11 +3,17 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
+
+
 
 const App = () => {
   return (
-    <Provider store={crateStore(reducers)}>
-      <View />
+    //Provider can only have 1 child node
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Tech Stack" />
+      </View>
     </Provider>
   );
 };
