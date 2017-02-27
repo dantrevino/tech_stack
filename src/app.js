50ad8aa60ee287f91be3ea53e040bf59c9e9e4ea
Dan Trevino <dantrevino@gmail.com>
2017-02-26 22:33:41 -0800

    add library list

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
-
+import LibraryList from './components/LibraryList'
 
 
 const App = () => {
@@ -13,6 +13,7 @@ const App = () => {
     <Provider store={createStore(reducers)}>
       <View>
         <Header headerText="Tech Stack" />
+        <LibraryList />
       </View>
     </Provider>
   );
