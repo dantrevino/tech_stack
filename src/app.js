7ead838137f2bacd49f74826de15fdb981dedb96
Dan Trevino <dantrevino@gmail.com>
2017-03-03 12:56:40 -0800

    Add selection reducer

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     //Provider can only have 1 child node
     <Provider store={createStore(reducers)}>
-      <View>
+      <View style={{ flex: 1}}>
         <Header headerText="Tech Stack" />
         <LibraryList />
       </View>
