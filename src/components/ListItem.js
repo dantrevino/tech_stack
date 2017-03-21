505d25458e77ce78e8b6590f5d1b08c968dae0da
Dan Trevino <dantrevino@gmail.com>
2017-03-20 21:46:11 -0700

    reducer update, lecture 100

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,17 +5,27 @@ import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription () {
+    const { libary, selectedLibraryID } = this.props;
+
+    if (expanded) {
+      return (
+        <Text>{ library.description }</Text>
+      );
+    }
+  }
+
   render() {
     const { titleStyle} = styles;
-
-    console.log(this.props);
+    const { id, title } = this.props.library;
 
     return (
-      <TouchableWithoutFeedback>
+      <TouchableWithoutFeedback onPress={()=> this.props.selectLibrary(id)}>
         <View>
           <CardSection>
-            <Text style={titleStyle}>{this.props.library.title}</Text>
+            <Text style={titleStyle}>{title}</Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -29,4 +39,10 @@ const styles = {
   }
 }
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryID === ownProps.library.id;
+
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
